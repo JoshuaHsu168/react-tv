665d8de350a0ff05674ae9845275318f1a71103d
Joshua Hsu <jshhsu168@gmail.com>
2025-07-08 03:23:18 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import { tvShows } from "./shows/data";
+import ShowSelection from "./shows/ShowSelection";
+import ShowDetails from "./shows/ShowDetails";
+import { useState } from "react";
+
 /**
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
 export default function App() {
+  const [allShows, setAllShows] = useState(tvShows);
+  const [selectedShow, setSelectedShow] = useState(tvShows[0]);
   return (
     <>
       <header>
         <p>React TV</p>
+        <ShowSelection shows={allShows} setSelectedShow={setSelectedShow} />
       </header>
-      <main></main>
+      <main>
+        <ShowDetails show={selectedShow} />
+      </main>
     </>
   );
 }
